11102d5bbefba0b387c42b0f3d76c2ade8819305
Tomas Hubelbauer <tomas@hubelbauer.net>
2019-06-12 20:45:54 +0200

    Handle single and double clicks of both the primary and secondary button

--- README.md
+++ README.md
@@ -6,12 +6,15 @@ I've built it for fun and as a detour from trying to figure out a frustrating pr
 
 I generally believe raster tiles are inferior to vector tiles, but boy, are they easier to work with!
 
+- Put back drawing strokes on the map, for now with antialiasing multiplication, in the future smarter with double buffer
+  canvas or by redrawing only the stroke portion of each tile, for each tile as it renders
+- Consider handling single primary and secondary clicks by carrying out the action and then reverting it if double click
+  as opposed to waiting to see if double click happened and only after determining it did not carrying out the action
+  (This could improve the perceived performance of the UI)
 - Fix the mobile error/panning error
-- Display a circle at the current location (initially the map center but it stays put with map panning and zooming) with accuracy radius
-- Add support for placing points by GPS coordinates (pixels to GPS calculation)
+- Change pins to points and have two kinds - location point with accuracy radius reflected at map size and manual one which is always 5px
 - Persist map configuration (longitude, latitude, zoom) after each change to restore where left off
   (Possibly combine this with refactoring the map code to a class `Map` with events like onTileLoad, onTileRender etc.)
-- Implement double left/right click to zoom in/out
 - Implement pinch to zoom
 - Improve the local storage cache so it evicts the least hit tile in favor or a new tile to persist
 - Use a service worker for caching the commonly hit tiles by using a logic which eventually evicts tiles with low hits
@@ -27,3 +30,5 @@ I generally believe raster tiles are inferior to vector tiles, but boy, are they
 - Use `OffscreenCanvas` in the tile cache if supported (right now only Chrome)
 - Consider implementing a rate-limiter algorithm in the tile cache so that the GitHub Pages version cannot be abused to slam the server
   (Although this would be impossible with the `img` based renderer which loads tiles itself - and a malicious person can still abuse)
+- Consider redrawing the whole canvas on each new tile load so that strokes and pins could be drawn without getting clipped
+  or antialiasing multiplying up by redrawing the whole stroke when just one of the tiles changed and others already had it drawn
--- index.js
+++ index.js
@@ -14,6 +14,12 @@ window.addEventListener('load', async () => {
       }, reject, { enableHighAccuracy: true })
     });
 
+  document.getElementById('centerCoordsSpan').textContent = `${longitude.toFixed(4)} ${latitude.toFixed(4)}`;
+  document.getElementById('zoomSpan').textContent = zoom;
+
+  const pins = [{ longitude, latitude }];
+  document.getElementById('pinsSpan').textContent = pins.length + (pins.length === 1 ? ' pin' : ' pins');
+
   const zoomInButton = document.getElementById('zoomInButton');
   zoomInButton.addEventListener('click', () => {
     if (zoom >= 18) {
@@ -21,6 +27,7 @@ window.addEventListener('load', async () => {
     }
 
     zoom++;
+    document.getElementById('zoomSpan').textContent = zoom;
     render();
   });
 
@@ -31,37 +38,135 @@ window.addEventListener('load', async () => {
     }
 
     zoom--;
+    document.getElementById('zoomSpan').textContent = zoom;
     render();
   });
 
   let mode = 'browse';
   const modeButton = document.getElementById('modeButton');
+  modeButton.textContent = mode === 'browse' ? 'Browsing mode. Switch to drawing' : 'Drawing mode. Switch to browsing';
+
   modeButton.addEventListener('click', () => {
     mode = mode === 'browse' ? 'draw' : 'browse';
+    modeButton.textContent = mode === 'browse' ? 'Browsing mode. Switch to drawing' : 'Drawing mode. Switch to browsing';
     render();
   });
 
   const mapCanvas = document.getElementById('mapCanvas');
 
+  let pointerX;
+  let pointerY;
+  let pointerLongitude;
+  let pointerLatitude;
+
+  const doubleClickThreshold = 250; // ms
   let strokes = [];
+  /** @type{(undefined | { button: number; timestamp: number; timeout: number; x: number; y: number; })} */ let lastClick;
   mapCanvas.addEventListener('pointerdown', event => {
     if (mode === 'draw') {
-      strokes.push([longitude, latitude, event.offsetX, event.offsetY]);
+      strokes.push([pointerLongitude, pointerLatitude]);
       return;
     }
+
+    let timeout;
+    switch (event.buttons) {
+      case 1: {
+        if (lastClick && lastClick.button === 1 && event.timeStamp - lastClick.timestamp < doubleClickThreshold) {
+          // Cancel the primary single click action as this turned out to be a double click
+          window.clearTimeout(lastClick.timeout);
+
+          if (zoom < 18) {
+            longitude = pointerLongitude;
+            latitude = pointerLatitude;
+            document.getElementById('centerCoordsSpan').textContent = `${longitude.toFixed(4)} ${latitude.toFixed(4)}`;
+
+            zoom++;
+            document.getElementById('zoomSpan').textContent = zoom;
+            render();
+          }
+        } else {
+          // Schedule a primary button click handler which will fire until this click turns into a double click
+          timeout = window.setTimeout(() => {
+            if (!lastClick || lastClick.x !== pointerX || lastClick.y !== pointerY) {
+              // Discard the single click as the mouse moved between the press and release (drag)
+              return;
+            }
+
+            pins.push({ longitude: pointerLongitude, latitude: pointerLatitude });
+            document.getElementById('pinsSpan').textContent = pins.length + (pins.length === 1 ? ' pin' : ' pins');
+            render();
+          }, doubleClickThreshold + 10);
+        }
+
+        break;
+      }
+      case 2: {
+        if (lastClick && lastClick.button === 2 && event.timeStamp - lastClick.timestamp < doubleClickThreshold) {
+          // Cancel the secondary single click action as this turned out to be a double click
+          window.clearTimeout(lastClick.timeout);
+
+          if (zoom > 0) {
+            longitude = pointerLongitude;
+            latitude = pointerLatitude;
+            document.getElementById('centerCoordsSpan').textContent = `${longitude.toFixed(4)} ${latitude.toFixed(4)}`;
+
+            zoom--;
+            document.getElementById('zoomSpan').textContent = zoom;
+            render();
+          }
+        } else {
+          // Schedule a secondary button click handler which will fire until this click turns into a double click
+          timeout = window.setTimeout(() => {
+            if (!lastClick || lastClick.x !== pointerX || lastClick.y !== pointerY) {
+              // Discard the single click as the mouse moved between the press and release (drag)
+              return;
+            }
+
+            longitude = pointerLongitude;
+            latitude = pointerLatitude;
+            document.getElementById('centerCoordsSpan').textContent = `${longitude.toFixed(4)} ${latitude.toFixed(4)}`;
+            render();
+          }, doubleClickThreshold + 10);
+        }
+
+        break;
+      }
+    }
+
+    lastClick = { button: event.buttons, timestamp: event.timeStamp, timeout, x: event.clientX, y: event.clientY };
   });
 
   mapCanvas.addEventListener('pointermove', event => {
+    pointerX = event.clientX;
+    pointerY = event.clientY;
+    document.getElementById('pointerPointsSpan').textContent = `${pointerX} ${pointerY}`;
+
+    // Find the center tile longitude and latitude indices (the integral part) and the ratio of the longitude and latitude within them (the fractional part)
+    const centerTileLongitudeNumber = (longitude + 180) / 360 * Math.pow(2, zoom);
+    const centerTileLatitudeNumber = (1 - Math.log(Math.tan(latitude * Math.PI / 180) + 1 / Math.cos(latitude * Math.PI / 180)) / Math.PI) / 2 * Math.pow(2, zoom);
+
+    const centerDifferenceX = pointerX - canvasWidth / 2;
+    const centerDifferenceY = pointerY - canvasHeight / 2;
+
+    // Transfer the difference in canvas pixels to a difference in tile numbers (multiples of tile size)
+    let pointerTileLongitudeNumber = centerTileLongitudeNumber + centerDifferenceX / tileWidth;
+    let pointerTileLatitudeNumber = centerTileLatitudeNumber + centerDifferenceY / tileHeight;
+
+    // Calculate the new longitude using the reserve formula plugging in the adjusted tile longitude number
+    pointerLongitude = pointerTileLongitudeNumber / Math.pow(2, zoom) * 360 - 180;
+
+    // Calculate the new latitude using the reserve formula plugging in the adjusted tile latitude number
+    const pointerN = Math.PI - 2 * Math.PI * pointerTileLatitudeNumber / Math.pow(2, zoom);
+    pointerLatitude = 180 / Math.PI * Math.atan(0.5 * (Math.exp(pointerN) - Math.exp(-pointerN)));
+
+    document.getElementById('pointerCoordsSpan').textContent = `${pointerLongitude.toFixed(4)} ${pointerLatitude.toFixed(4)}`;
+
     if (event.buttons === 1) {
       if (mode === 'draw') {
         strokes[strokes.length - 1].push(event.offsetX, event.offsetY);
         render();
         return;
       }
-
-      // Find the center tile longitude and latitude indices (the integral part) and the ratio of the longitude and latitude within them (the fractional part)
-      const centerTileLongitudeNumber = (longitude + 180) / 360 * Math.pow(2, zoom);
-      const centerTileLatitudeNumber = (1 - Math.log(Math.tan(latitude * Math.PI / 180) + 1 / Math.cos(latitude * Math.PI / 180)) / Math.PI) / 2 * Math.pow(2, zoom);
 
       // Transfer the change in canvas pixels to a change in tile numbers (multiples of tile size)
       let newCenterTileLongitudeNumber = centerTileLongitudeNumber + -event.movementX / tileWidth;
@@ -74,8 +179,13 @@ window.addEventListener('load', async () => {
       const n = Math.PI - 2 * Math.PI * newCenterTileLatitudeNumber / Math.pow(2, zoom);
       latitude = 180 / Math.PI * Math.atan(0.5 * (Math.exp(n) - Math.exp(-n)));
 
+      document.getElementById('centerCoordsSpan').textContent = `${longitude.toFixed(4)} ${latitude.toFixed(4)}`;
       render();
     }
+  });
+
+  mapCanvas.addEventListener('contextmenu', event => {
+    event.preventDefault();
   });
 
   window.addEventListener('resize', render);
@@ -168,6 +278,25 @@ window.addEventListener('load', async () => {
             // Draw the tile image
             context.drawImage(tile, tileCanvasX, tileCanvasY);
 
+            // Find pins on this tile
+            for (const pin of pins) {
+              const longitudeNumber = (pin.longitude + 180) / 360 * Math.pow(2, zoom);
+              const longitudeIndex = Math.floor(longitudeNumber);
+              const longitudeRatio = longitudeNumber % 1;
+              const latitudeNumber = (1 - Math.log(Math.tan(pin.latitude * Math.PI / 180) + 1 / Math.cos(pin.latitude * Math.PI / 180)) / Math.PI) / 2 * Math.pow(2, zoom);
+              const latitudeIndex = Math.floor(latitudeNumber);
+              const latitudeRatio = latitudeNumber % 1;
+
+              // Draw the pin if it belongs to this tile
+              // TODO: Address the ticket which deals with pins clipping if they are closer to the edge of the tile than their radius
+              if (tileLongitudeIndex === longitudeIndex && tileLatitudeIndex === latitudeIndex) {
+                context.fillStyle = 'blue';
+                context.beginPath();
+                context.arc(tileCanvasX + longitudeRatio * tileWidth, tileCanvasY + latitudeRatio * tileHeight, 10, 0, Math.PI * 2);
+                context.fill();
+              }
+            }
+
             // TODO: Find the lines and line portions within this tile and redraw them
           })
           .catch(error => {
@@ -187,16 +316,7 @@ window.addEventListener('load', async () => {
 
     zoomInButton.disabled = zoom >= 18;
     zoomOutButton.disabled = zoom <= 0;
-    const a = document.getElementById('tileCooordSpan');
-    tileCooordSpan.textContent = '';
-    tileCooordSpan.textContent = longitude.toFixed(4);
-    tileCooordSpan.textContent += ' ';
-    tileCooordSpan.textContent += latitude.toFixed(4);
-    tileCooordSpan.textContent += ' | ';
-    tileCooordSpan.textContent += zoom;
-    tileCooordSpan.textContent += ' | ';
-    tileCooordSpan.textContent += `${centerTileLongitudeIndex} ${centerTileLatitudeIndex} | ${leftColumnTilesLongitudeIndex}+${horizontalTileCount} ${topRowTilesLatitudeIndex}+${verticalTileCount}`;
-    modeButton.textContent = mode === 'browse' ? 'Browsing mode. Switch to drawing' : 'Drawing mode. Switch to browsing';
+    document.getElementById('tilesSpan').textContent = `${centerTileLongitudeIndex} ${centerTileLatitudeIndex} | ${leftColumnTilesLongitudeIndex}+${horizontalTileCount} ${topRowTilesLatitudeIndex}+${verticalTileCount}`;
   }
 
   // Render the initial map view
